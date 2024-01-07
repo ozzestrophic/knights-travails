7e5cff13b415ea2d4e1f70f684851f4c873e81ab
Ozzeek <osama.m.alnagar@gmail.com>
2024-01-07 04:28:12 +0200

    refactor: changed names for better readability

--- index.js
+++ index.js
@@ -37,41 +37,37 @@ function createMoves(square) {
   return moves;
 }
 
-function BFSsquares(square1, square2) {
+function knightMoves(square1, square2) {
   const q = [[square1]];
 
   const visited = new Set();
 
   while (q.length > 0) {
     const currentSeq = q.shift();
-    console.log("looping seq", currentSeq);
     const currentSquare = currentSeq[currentSeq.length - 1];
-    console.log("current square ", currentSquare);
     visited.add(currentSquare.toString());
 
     const square1Moves = createMoves(currentSquare);
 
     for (const move of square1Moves) {
-      console.log("move is ", move);
-
       if (!visited.has(move.toString())) {
         q.push([...currentSeq, move]);
-        console.log("looping ----");
       }
 
       if (move.toString() === square2.toString()) {
         currentSeq.push(square2);
-        console.log(`found in ${currentSeq.length} moves`);
-        console.log(currentSeq);
+        printMoves(currentSeq);
         return currentSeq;
       }
     }
   }
 }
 
-BFSsquares([0, 0], [1, 4]);
+function printMoves(sequence) {
+  console.log(`Made it in ${sequence.length} moves! Here is your path:`);
+  for (const array of sequence) {
+    console.log(array);
+  }
+}
 
-// const newArr = [12, [12]];
-// newArr.push([...additions, [12, 12]]);
-
-// console.table(newArr);
+knightMoves([0, 0], [1, 4]);
